bd5553f0b74c82425e9e89ee490e9dbdfbf5a121
rirefat <rirefat.official@gmail.com>
2024-01-25 05:00:20 +0600

    done header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header/Header";
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem"
+      },
+      colors: {
+        primary: "#7f30f7"
+      }
+    },
   },
   plugins: [],
 }
