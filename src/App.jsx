0daab2d24a21524dd21a08440da8adf590003e25
rirefat <rirefat.official@gmail.com>
2024-01-25 07:22:28 +0600

    almost done movie card except ratings section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import MovieList from "./components/MovieList/MovieList";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <main className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
         <Sidebar />
+        <MovieList/>
       </main>
       <Footer />
     </>
